9bf5ece016f4d38aa0c7d5d07b08dea488660f42
Gyuwon Yi <gyuwon@live.com>
2020-12-09 20:08:00 +0900

    Initialize project

---
+++ Sample/Samples.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sample
+{
+    [TestClass]
+    public class Samples
+    {
+        public TestContext TestContext { get; set; }
+    }
+}
