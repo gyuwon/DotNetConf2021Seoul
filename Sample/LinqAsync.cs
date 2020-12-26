71430163873c229bc7cecf7c07a528955a63071f
Gyuwon Yi <gyuwon@live.com>
2020-12-26 20:57:20 +0900

    Add System.Linq.Async sample

---
+++ Sample/LinqAsync.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    [TestClass]
+    public class LinqAsync
+    {
+        public TestContext TestContext { get; set; }
+
+        private async IAsyncEnumerable<int> Get([EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(millisecondsDelay: 100);
+                yield return i;
+            }
+        }
+
+        [TestMethod]
+        public async Task UseSelect()
+        {
+            int result = await Get().Select(Square).SumAsync();
+            TestContext.WriteLine($"Result: {result}");
+        }
+
+        private int Square(int x) => x * x;
+
+        [TestMethod]
+        public async Task UseSelectAwait()
+        {
+            int result = await Get().SelectAwait(SquareAsync).SumAsync();
+            TestContext.WriteLine($"Result: {result}");
+        }
+
+        private async ValueTask<int> SquareAsync(int x)
+        {
+            await Task.Delay(millisecondsDelay: 100);
+            return x * x;
+        }
+
+        [TestMethod]
+        public async Task Parallelism()
+        {
+            List<Task<int>> tasks = await Get().Select(SquareAsync)
+                                               .Select(valueTask => valueTask.AsTask())
+                                               .ToListAsync();
+            int[] squares = await Task.WhenAll(tasks);
+            int result = squares.Sum();
+            TestContext.WriteLine($"Result: {result}");
+        }
+    }
+}
