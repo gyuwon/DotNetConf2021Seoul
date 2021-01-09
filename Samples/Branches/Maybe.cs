57db8424a8fd5f4926f431fbb9b45510ac5350e4
Gyuwon Yi <gyuwon@live.com>
2021-01-09 14:32:09 +0900

    Add Maybe<T> sample

---
+++ Samples/Branches/Maybe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sample
+{
+    public class Maybe<T>
+    {
+        private readonly bool hasValue;
+        private readonly T value;
+
+        private Maybe() => hasValue = false;
+
+        internal Maybe(T value)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            (hasValue, this.value) = (true, value);
+        }
+
+        public static Maybe<T> Nothing { get; } = new Maybe<T>();
+
+        public Maybe<U> Map<U>(Func<T, U> map)
+            => hasValue ? new Maybe<U>(map.Invoke(value)) : Maybe<U>.Nothing;
+
+        public Maybe<U> Bind<U>(Func<T, Maybe<U>> map)
+            => hasValue ? map.Invoke(value) : Maybe<U>.Nothing;
+    }
+
+    public static class Maybe
+    {
+        public static Maybe<T> Just<T>(T value)
+            => new Maybe<T>(value);
+
+        public static Maybe<T> Create<T>(T value) where T : class
+            => value == null ? Maybe<T>.Nothing : Just(value);
+    }
+}
---
+++ Samples/Branches/SumType.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sample
+{
+    [TestClass]
+    public class SumType
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Just()
+        {
+            Maybe<int> maybe = Maybe.Just(1);
+            maybe.DoIfJust(value => TestContext.WriteLine($"Just {value}"));
+        }
+
+        [TestMethod]
+        public void Nothing()
+        {
+            Maybe<int> maybe = Maybe<int>.Nothing;
+            maybe.Do(forJust: _ => { }, forNothing: () => TestContext.WriteLine("Nothing"));
+        }
+
+        [TestMethod]
+        [DataRow("foo", "bar")]
+        [DataRow(null, "bar")]
+        [DataRow("foo", null)]
+        [DataRow(null, null)]
+        public void Concat(string a, string b)
+        {
+            var ma = Maybe.Create(a);
+            var mb = Maybe.Create(b);
+            Maybe<string> result = ma.Bind(x => mb.Map(y => $"{x}{y}"));
+
+            result.Do(forJust: value => TestContext.WriteLine($"Just {value}"),
+                      forNothing: () => TestContext.WriteLine("Nothing"));
+        }
+
+        [TestMethod]
+        [DataRow("foo", "bar")]
+        [DataRow(null, "bar")]
+        [DataRow("foo", null)]
+        [DataRow(null, null)]
+        public void ConcatLinq(string a, string b)
+        {
+            Maybe<string> result = from x in Maybe.Create(a)
+                                   from y in Maybe.Create(b)
+                                   select $"{x}{y}";
+
+            result.Do(forJust: value => TestContext.WriteLine($"Just {value}"),
+                      forNothing: () => TestContext.WriteLine("Nothing"));
+        }
+    }
+}
+ 
